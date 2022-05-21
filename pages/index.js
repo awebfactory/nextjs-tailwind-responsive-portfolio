2e99ca95aaef9b5c0a590996234bc0a1ce87fcb7
Victor Kane <victorkane@gmail.com>
2022-05-21 16:51:30 -0300

    feat: Added Services Section

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head"
 import Navbar from "../components/Navbar"
 import HomePage from "./homePage"
+import Services from "./services"
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       </Head>
       <Navbar />
       <HomePage />
+      <Services />
     </div>
   )
 }
